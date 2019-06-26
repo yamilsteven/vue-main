cff60605366aba79119f085a2bdc1ece690ee820
Jose Caicedo <jocaicedo@hugeinc.com>
2019-06-26 16:10:42 -0500

    Props example

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,38 +1,50 @@
 <template>
   <div class="fields">
-    <input type="text" class="song-input" :placeholder="songPlaceholder" v-model="newSong" @keyup.enter="addSong">
-    <input type="text" class="song-input" :placeholder="artistPlaceholder" v-model="newArtist" @keyup.enter="addSong">
+    <input
+      type="text"
+      class="song-input"
+      :placeholder="songPlaceholder"
+      v-model="newSong"
+      @keyup.enter="addSong"
+    >
+    <input
+      type="text"
+      class="song-input"
+      :placeholder="artistPlaceholder"
+      v-model="newArtist"
+      @keyup.enter="addSong"
+    >
   </div>
 </template>
 
 <script>
-import Vue from 'vue';
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'Form',
+  name: "Form",
   data() {
     return {
-      songPlaceholder: 'New Song',
-      artistPlaceholder: 'New Artist',
-      newSong: '',
-      newArtist: ''
-    }
+      songPlaceholder: "New Song",
+      artistPlaceholder: "New Artist",
+      newSong: "",
+      newArtist: ""
+    };
   },
   methods: {
     addSong() {
-      if(!this.newSong.trim().length || !this.newArtist.trim().length) {
-          return;
+      if (!this.newSong.trim().length || !this.newArtist.trim().length) {
+        return;
       }
 
       const newSongObj = {
-          id: new Date().getTime(),
-          title: this.newSong,
-          artist: this.newArtist,
-          score: 0
+        id: new Date().getTime(),
+        title: this.newSong,
+        artist: this.newArtist,
+        score: 0
       };
-      
-      this.$emit('create', newSongObj);
-    },
+
+      this.$emit("create", newSongObj);
+    }
   }
 });
 </script>
--- src/components/Song.vue
+++ src/components/Song.vue
@@ -1,54 +1,45 @@
 <template>
   <div class="song-item">
-    <div class="song-title">
-        {{ song.title }} - {{ song.artist }}
-    </div>
+    <div class="song-title">{{ song.title }} - {{ song.artist }}</div>
     <img class="thumb" @click="likeSong($event)" alt="logo" src="../assets/thumb.png">
-    <img class="thumb thumb--down" @click="dislikeSong($event)" alt="logo" src="../assets/thumb.png">
-    <div>
-        Score: {{ song.score }}
-    </div>
-    <div class="remove-song" @click="removeSong()">
-        &times;
-    </div>
+    <img
+      class="thumb thumb--down"
+      @click="dislikeSong($event)"
+      alt="logo"
+      src="../assets/thumb.png"
+    >
+    <div>Score: {{ song.score }}</div>
+    <div class="remove-song" @click="removeSong()">&times;</div>
   </div>
 </template>
 
 <script>
-import Vue from 'vue';
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'Song',
+  name: "Song",
 
-  props: {
-    item: {
-      default: {},
-      type: Object
-    }
-  },
   data() {
     return {
-      song: this.item,
-    }
+      song: {}
+    };
   },
   methods: {
     removeSong() {
-      this.$emit('remove');
+      this.$emit("remove");
     },
     likeSong: function(event) {
       if (event) {
-        this.song.score++;
         this.updateSong();
       }
     },
     dislikeSong: function(event) {
-      if (this.song.score && event) {
-        this.song.score--;
+      if (event) {
         this.updateSong();
       }
     },
     updateSong() {
-      this.$emit('update');
+      this.$emit("update");
     }
   }
 });
